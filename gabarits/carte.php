40a0747ccc04e812e615d600fb29195c03b37049
AbeviMalaika <e2395930@cmaisonneuve.qc.ca>
2025-03-10 11:43:38 -0400

    s7-c2 - Travail de la présentation des cartes de destinations (structure HTML et scss)

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -4,18 +4,20 @@
 */
 ?>
 <article class="carte carte--grande">
-    <figure class="carte__image">
+    <!-- <figure class="carte__image">
         <img src="voyage.jpg" alt="Image de voyage">
-    </figure>
+    </figure> -->
   <div class="carte__contenu">
     <?php
       if (has_post_thumbnail()) {
       the_post_thumbnail('thumbnail'); }
     ?>
     <h4 class="carte__titre"><?php the_title(); ?></h4>
-    <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 10, "..."); ?></p>
-    <a class="carte__bouton carte__bouton--actif" href="<?= the_permalink() ?>">Voir plus...</a>
     <?= the_category(); ?>
-    <p>Température maximum : <?= the_field('temperature_maximum'); ?> C&#176;</p>
+    <div class="carte__conteneurinfosgenerales">
+      <p class="carte__description"><?php echo wp_trim_words(get_the_content(), 25, "..."); ?></p>
+      <a class="carte__bouton carte__bouton--actif" href="<?= the_permalink() ?>">Voir plus...</a>
+      <p class="carte__temperature">Température maximum : <?= the_field('temperature_maximum'); ?> C&#176;</p>
+    </div>
   </div>
 </article>
