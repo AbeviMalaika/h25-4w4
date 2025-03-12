dbfa554e266037ea7f506da2a0ead673bc99d0e3
Abevi <e2395930@cmaisonneuve.qc.ca>
2025-03-12 17:22:26 -0400

    s8-c1 - Ajout de la section footer dans le customizer et début de l'implémentation des valeurs du customizer dans le code HTML

--- functions/customizer.php
+++ functions/customizer.php
@@ -1,6 +1,7 @@
 <?php
 function theme_4w4_customize_register($wp_customize)
 {
+    /*----------------SECTION HERO-----------------*/
     // Le code pour ajouter des sections, des réglages et des contrôles ira ici.
     // Création d'une nouvelle section dans le customizer
     ///////////////////////////////////////////////////////////////Début de la zone hero
@@ -32,30 +33,99 @@ function theme_4w4_customize_register($wp_customize)
     ));
     ////////////////////////////////////////// ajout du contrôle de la donnée image en background
     $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_background', array(
-        'label' => __('Image Arrière-plan Hero', 'theme_31w'),
+        'label' => __('Image Arrière-plan Hero', 'theme_4w4'),
         'section' => 'hero_section',
     )));
 
 
+    /**********/////////////////////////////////////////////////Début du champ hero_courriel*/
+    ////////////////////////////////////////// ajout de la donnée
+    $wp_customize->add_setting('hero_courriel', array(
+        'default' => __('malaika.abevi@gmail.com', 'theme_4w4'),
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    ////////////////////////////////////////// ajout du contrôle de la donnée
+    $wp_customize->add_control('hero_courriel', array(
+        'label' => __('Courriel', 'theme_4w4'),
+        'section' => 'hero_section',
+        'type' => 'text',
+    ));
 
 
+    /**********/////////////////////////////////////////////////Début du champ hero_texteColo*/
+    ////////////////////////////////////////// ajout de la donnée de couleur des icones
+    $wp_customize->add_setting( 'hero_texteColo', array(
+        'default' => '#ffffff',
+        'sanitize_callback' => 'sanitize_hex_color',
+    ));
+    $wp_customize->add_control(
+    new WP_Customize_Color_Control( $wp_customize, 'hero_texteColo', array(
+        'label' => __( 'Couleur Texte' ),
+        'section' => 'hero_section',
+    )));
 
+
+    /**********/////////////////////////////////////////////////Début du champ hero_icones*/
     ////////////////////////////////////////// ajout de la donnée de couleur des icones
     $wp_customize->add_setting( 'hero_icones', array(
+        'default' => 'ffffff',
         'sanitize_callback' => 'sanitize_hex_color_no_hash',
     ));
-    
-
     $wp_customize->add_control(
-    new WP_Customize_Color_Control( $wp_customize, 'hero_icones',
-    array(
+    new WP_Customize_Color_Control( $wp_customize, 'hero_icones', array(
         'label' => __( 'Couleur Îcones Sociaux' ),
         'section' => 'hero_section',
     )));
       
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+    /*----------------SECTION FOOTER-----------------*/
+    // Création d'une nouvelle section dans le customizer
+    ///////////////////////////////////////////////////////////////Début de la zone footer
+    $wp_customize->add_section('footer_section', array(
+        'title' => __('Section Footer', 'theme_4w4'),
+        'priority' => 30,
+    ));
+
+
+    ////////////////////////////////////////// ajout de la donnée
+    $wp_customize->add_setting('footer_telephone', array(
+        'default' => __('123-456-789', 'theme_4w4'),
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    ////////////////////////////////////////// ajout du contrôle de la donnée
+    $wp_customize->add_control('footer_telephone', array(
+        'label' => __('Numéro de Téléphone', 'theme_4w4'),
+        'section' => 'footer_section',
+        'type' => 'text',
+    ));
+
+
+    ////////////////////////////////////////// ajout de la donnée
+    $wp_customize->add_setting('footer_adresse', array(
+        'default' => __('1234 rue Sherbrooke', 'theme_4w4'),
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    ////////////////////////////////////////// ajout du contrôle de la donnée
+    $wp_customize->add_control('footer_adresse', array(
+        'label' => __('Adresse', 'theme_4w4'),
+        'section' => 'footer_section',
+        'type' => 'text',
+    ));
+
+
+    ////////////////////////////////////////// ajout de la donnée
+    $wp_customize->add_setting('footer_mission', array(
+        'default' => __('Courte description de la mission du club de voyage', 'theme_4w4'),
+        'sanitize_callback' => 'sanitize_text_field'
+    ));
+    ////////////////////////////////////////// ajout du contrôle de la donnée
+    $wp_customize->add_control('footer_mission', array(
+        'label' => __('Mission', 'theme_4w4'),
+        'section' => 'footer_section',
+        'type' => 'textarea',
+    ));
 }
 
 add_action('customize_register', 'theme_4w4_customize_register');
-
 ?>
--- gabarits/hero.php
+++ gabarits/hero.php
@@ -6,15 +6,16 @@
 <?php 
 $hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
 $hero_background = get_theme_mod('hero_background', ''); 
+$hero_texteColo = get_theme_mod('hero_texteColo', '');
 ?>
 <section class="hero" style="background-image: url(<?php echo $hero_background; ?>);"> 
     <div class="hero__contenu global">
         <h1 class="hero__titre"><?= bloginfo('name')  ?></h1>
-        <p class="hero__description">
+        <p class="hero__description" style="color: <?php echo $hero_texteColo; ?>;">
         <?= bloginfo('description')  ?>
         </p>
         <p class="hero__auteur">
-            Malaïka Abevi
+            Autrice : <?php echo $hero_auteur; ?>
         </p>
         <p class="hero__courriel">
             <a href="#"> <?= bloginfo('admin_email')  ?></a>
@@ -25,7 +26,6 @@ $hero_background = get_theme_mod('hero_background', '');
         <p class="hero__telephone">
             514 254-7131
         </p>
-        <p class="hero__auteur">Auteur : <?php echo $hero_auteur; ?></p>
         <button class="hero__bntinscription">S'inscrire</button>
         <?php get_template_part('gabarits/icones-sociaux'); ?>
     </div>
