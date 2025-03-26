6e2cce45b6bc21e9a22fca48f7c33f17004fbeb4
AbeviMalaika <e2395930@cmaisonneuve.qc.ca>
2025-03-25 23:54:14 -0400

    amelioration du SCSS pour la page 404

--- 404.php
+++ 404.php
@@ -10,7 +10,8 @@ $error_background = get_theme_mod('404_background', '')
 
 <?php get_header() ?>
 <div class="erreur" style="background-image: linear-gradient(180deg, rgba(250, 214, 214, 0.27) 0%, rgba(105, 208, 204, 0.5) 89%, rgba(66, 207, 200, 0.25) 100%), url(<?php echo $error_background; ?>);">
-    <h1 class="erreur__titre"><?php echo $error_titre; ?></h1> <!-- Erreur 404 : Vous n'avez pas atterri à la bonne destination... -->
+    <h1 class="erreur__titre"><?php echo $error_titre; ?></h1>
+    <h2 class="erreur__sous-titre">Vous n'avez pas atterri à la bonne destination...</h2>
     <div class="erreur__message">
             <img class="erreur__message__imgAvion" src="<?php echo $error_avion; ?>" alt="">
             <!-- <img class="erreur__message__imgIle" src="<?php echo $error_imgprincipale; ?>" alt=""> -->
@@ -24,7 +25,7 @@ $error_background = get_theme_mod('404_background', '')
             <?php get_template_part("gabarits/icones-sociaux"); ?>
         </div>
         <div class="erreur__navigation">
-            <h4 class="erreur__navigation">Une de ses destinations pourrait vous conquérir</h4>
+            <h4 class="erreur__navigation_titre">Une de ses destinations pourrait vous conquérir</h4>
             <?php wp_nav_menu(array(
                 "menu" => "recommandation",
                 "container" => "nav",
