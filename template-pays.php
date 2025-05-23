557630bf8a5b6643c019d30175dea73069e69023
Malaïka Abevi <151085720+AbeviMalaika@users.noreply.github.com>
2025-05-23 09:33:16 -0400

    Examen final - Ajout de la rest api pour afficher les destinations selon les pays

--- template-pays.php
+++ template-pays.php
@@ -24,12 +24,30 @@ get_header();
     </p>
 </section>
 
-<?php genere_vague($footer_couleur, "100%", 35); ?>
+<?php creer_vague("blue", "red"); ?>
 
 <!-- ////////////////////////////////////////////////////// Section Rest API -->
 <section class="destination">
-    <?php categories_liste("destination"); ?>
-    <h2 class="destination__titre">Articles de la catégorie</h2>
+
+    <ul class="categorie__ul">
+        <li data-destinationID="france" tabindex="1" class=" categorie__ul__li">France</li>
+        <li data-destinationID="etats-unis" tabindex="1" class=" categorie__ul__li">États-Unis</li>
+        <li data-destinationID="canada" tabindex="1" class=" categorie__ul__li">Canada</li>
+        <li data-destinationID="argentine" tabindex="1" class=" categorie__ul__li">Argentine</li>
+        <li data-destinationID="chili" tabindex="1" class=" categorie__ul__li">Chili</li>
+        <li data-destinationID="belgique" tabindex="1" class=" categorie__ul__li">Belgique</li>
+        <li data-destinationID="maroc" tabindex="1" class=" categorie__ul__li">Maroc</li>
+        <li data-destinationID="mexique" tabindex="1" class=" categorie__ul__li">Mexique</li>
+        <li data-destinationID="japon" tabindex="1" class=" categorie__ul__li">Japon</li>
+        <li data-destinationID="italie" tabindex="1" class=" categorie__ul__li">Italie</li>
+        <li data-destinationID="islande" tabindex="1" class=" categorie__ul__li">Islande</li>
+        <li data-destinationID="chine" tabindex="1" class=" categorie__ul__li">Chine</li>
+        <li data-destinationID="grece" tabindex="1" class=" categorie__ul__li">Grèce</li>
+        <li data-destinationID="Suisse" tabindex="1" class=" categorie__ul__li">Suisse</li>
+    </ul>
+
+
+    <!-- <h2 class="destination__titre">Articles de la catégorie</h2> -->
     <div class="destination__list"></div>
 </section>
 
